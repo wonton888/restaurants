e5c6e2ea82951493eba65f8396ba52a8bb1c6406
Rick Hills & Samantha Maturen <student@epicodus.com>
2015-08-19 11:40:13 -0700

    Add getters and setters in Cuisine class, add save, getAll, deleteAll function in Cuisine class

--- src/Cuisine.php
+++ src/Cuisine.php
@@ -11,15 +11,51 @@
 
         }
 
+        function setId($new_id)
+        {
+            $this->id = $new_id;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function setType($new_type)
+        {
+            $this->type = (string) $new_type;
+        }
+
+        function getType()
+        {
+            return $this->type;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO cuisines (type) VALUES ('{$this->getType()}')");
+            $result_id = $GLOBALS['DB']->lastInsertId();
+            $this->setId($result_id);
+        }
+
+        static function getAll()
+        {
+            $returned_cuisines = $GLOBALS['DB']->query("SELECT * FROM cuisines;");
+            $cuisines = array();
+            foreach ($returned_cuisines as $cuisine) {
+                $type = $cuisine['type'];
+                $id = $cuisine['id'];
+                $new_cuisine = new Cuisine($type, $id);
+                array_push($cuisines, $new_cuisine);
+            }
+
+            return $cuisines;
+        }
+
         static function deleteAll()
         {
             $GLOBALS['DB']->exec("DELETE FROM cuisines;");
         }
-
-
-
-
-
-
-
+        
     }
+?>
--- tests/CuisineTest.php
+++ tests/CuisineTest.php
@@ -22,7 +22,40 @@
             Restaurant::deleteAll();
         }
 
+        function test_deleteAll()
+        {
+            //Arrange
+            $type = "Italian";
+            $type2 = "Japanese";
+            $test_Cuisine = new Cuisine($type);
+            $test_Cuisine->save();
+            $test_Cuisine2 = new Cuisine($type2);
+            $test_Cuisine2->save();
 
+            //Act
+            Cuisine::deleteAll();
+            $result = Cuisine::getAll();
+
+            //Assert
+            $this->assertEquals([], $result);
+        }
+
+        // function test_getAll()
+        // {
+        //     //Arrange
+        //     $type = "Work stuff";
+        //     $type2 = "Home stuff";
+        //     $test_Category = new Category($type);
+        //     $test_Category->save();
+        //     $test_Category2 = new Category($type2);
+        //     $test_Category2->save();
+        //
+        //     //Act
+        //     $result = Category::getAll();
+        //
+        //     //Assert
+        //     $this->assertEquals([$test_Category, $test_Category2], $result);
+        // }
 
     }
 
