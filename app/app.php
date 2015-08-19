10f5a07c6b687e7dfadd0a10c43597b156f2033a
Rick Hills & Samantha Maturen <student@epicodus.com>
2015-08-19 16:09:31 -0700

    Add get, patch, and post routes for app.php

---
+++ app/app.php
@@ -0,0 +1,63 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Restaurant.php";
+    require_once __DIR__."/../src/Cuisine.php";
+
+    $app = new Silex\Application();
+
+    $app['debug'] = true;
+
+    $server = 'mysql:host=localhost;dbname=restaurant_reviews';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig',
+        array('cuisines' => Cuisine::getAll()));
+    });
+
+    $app->post("/cuisines", function() use ($app) {
+            $cuisine = new Cuisine($_POST['type']);
+            $cuisine->save();
+            return $app['twig']->render('index.html.twig',
+            array('cuisines' => Cuisine::getAll()));
+    });
+
+    $app->post("/delete_cuisines", function() use ($app) {
+        Cuisine::deleteAll();
+        return $app['twig']->render('delete_cuisines.html.twig');
+    });
+
+    $app->get("/cuisines/{id}", function($id) use ($app) {
+        $cuisine = Cuisine::find($id);
+        return $app['twig']->render('cuisine_edit.html.twig',
+        array('cuisine' => $cuisine));
+    });
+
+    $app->patch("/cuisines/{id}", function($id) use ($app) {
+        $type = $_POST['type'];
+        $cuisine = Cuisine::find($id);
+        $cuisine->update($type);
+        return $app['twig']->render('cuisine.html.twig',
+        array('cuisine' => $cuisine, 'restaurants' => $restaurant->getRestaurants()));
+    });
+
+    $app->delete("/cuisines/{id}", function($id) use ($app) {
+        $cuisine = Cuisine::find($id);
+        $cuisine->delete();
+        return $app['twig']->render('index.html.twig',
+        array('cuisines' => Cuisine::getAll()));
+    });
+
+
+    return $app;
+
+?>
